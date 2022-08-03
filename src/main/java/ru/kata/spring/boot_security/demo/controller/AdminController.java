102630c41bc6faaa40f360333e6bc73841b54fbc
RibbleStripe <ribblestripeps3@gmail.com>
2022-08-04 02:00:40 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,81 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+    @Autowired
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping()
+    public String index(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        model.addAttribute("user", userService.getUserByName(userDetails.getUsername()));
+        model.addAttribute("users", userService.getAllUsers());
+        return "admin";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(@PathVariable(value = "id", required = false) Long id, Model model) {
+        User user = userService.getUserById(id);
+        if (user == null) {
+            return "redirect:/admin";
+        }
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "edit";
+    }
+
+    @PatchMapping("/{id}/update")
+    public String updateUser(@PathVariable(value = "id", required = false) Long id,
+                             @ModelAttribute("user") User user,
+                             @RequestParam(value = "namerole", required = false) String namerole,
+                             @RequestParam(value = "password", required = false) String password) {
+        user.setRoles(roleService.getByName(namerole));
+        user.setPassword(password);
+        userService.update(user, id);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public String deleteUser(@PathVariable(value = "id", required = false) Long id) {
+        userService.delete(id);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/create")
+    public String createPage(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String createUser(@ModelAttribute("user") User user,
+                             @RequestParam(value = "name", required = false) String namerole,
+                             @RequestParam(value = "username") String username) {
+        user.setUsername(username);
+        user.setRoles(roleService.getByName(namerole));
+        userService.add(user);
+        return "redirect:/admin";
+    }
+
+
+
+
+}
